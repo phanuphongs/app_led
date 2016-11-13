f0fa542cc537fc5abc5560864a2fd45268b8d10e
phanuphongs <patricksuphalawut@Patricks-MacBook-Pro.local>
2016-11-13 17:47:34 +0700

    keypad

--- main.cpp
+++ main.cpp
@@ -5,24 +5,19 @@
 
 PwmOut PWM1(P2_0);
 
-DigitalIn buttonUp1(P0_8);
-DigitalIn buttonUp2(P0_9);
-DigitalIn buttonDown2(P0_7);
-DigitalIn buttonDown3(P0_6);
+InterruptIn buttonUp1(P0_6);
+InterruptIn buttonUp2(P0_7);
+InterruptIn buttonDown2(P0_8);
+InterruptIn buttonDown3(P0_9);
 
-DigitalIn optoSensor(P0_18);
+InterruptIn optoSensor(P0_18);
 
-DigitalIn KeyPadRow1(P0_25);
-DigitalIn KeyPadRow2(P0_26);
-DigitalIn KeyPadRow3(P0_2);
-DigitalIn KeyPadRow4(P0_3);
-DigitalIn KeyPadCol1(P0_21);
-DigitalIn KeyPadCol1(P0_22);
-DigitalIn KeyPadCol1(P0_27);
-DigitalIn KeyPadCol1(P0_28);
+DigitalOut keyPadRow1(P0_21);
 
-InterruptIn button(P0_22);
-InterruptIn button2(P0_27);
+InterruptIn keyPadCol1(P0_28);
+InterruptIn keyPadCol2(P0_27);
+InterruptIn keyPadCol3(P0_22);
+
 bool isElevatorOn = false;
 int currentLevel = 1;
 int currentDirection = UP;
@@ -30,15 +25,18 @@ bool request[3][2] = {{false, false,},
 				{false, false},
 				{false, false}};
 bool destination[3] = {false, false, false};
-int distanceCount = 0;
+int distanceCount = 1;
 int possibleStop = 0;
 int nonPossibleStop = 0;
+bool keyPadInterruptState = false;
+
 
 void callElevator(int level){
 	if(!isElevatorOn){
 		isElevatorOn = true;
 		if(currentLevel == level){
 			//door open
+			isElevatorOn = false;
 		}else if(currentLevel > level){
 			currentDirection = DOWN;
 			PWM1.pulsewidth_us(2000);
@@ -65,37 +63,56 @@ int checkLimitDirection(){
 }
 
 void addStop(int reqLevel){
+	if(reqLevel == currentLevel)
+			return;
 	if(currentDirection == UP & currentLevel+1 <= reqLevel || currentDirection == DOWN & currentLevel-1 >= reqLevel){
-		if(request[reqLevel][currentDirection] == false && destination[reqLevel] == false)
+		if(request[reqLevel-1][currentDirection] == false && destination[reqLevel-1] == false)
 			possibleStop++;
 		return;
 	}
-	if(request[reqLevel][currentDirection ? 0 : 1] == false && destination[reqLevel] == false);
+	if(request[reqLevel-1][currentDirection ? 0 : 1] == false && destination[reqLevel-1] == false);
 		nonPossibleStop++;
 }
 
 void addStop(int reqLevel, int direction){
+	if(reqLevel == currentLevel)
+		return;
 	if(direction == currentDirection){
-		if(currentDirection == UP & currentLevel+1 <= reqLevel || currentDirection == DOWN & currentLevel-1 >= reqLevel){
-			if(request[reqLevel][direction] == false && destination[reqLevel] == false)
+		if(currentDirection == UP & currentLevel+1 < reqLevel || currentDirection == DOWN & currentLevel-1 > reqLevel){
+			if(request[reqLevel-1][direction] == false && destination[reqLevel-1] == false)
 				possibleStop++;
 			return;
 		}
 	}
-	if(request[reqLevel][direction ? 0 : 1] == false && destination[reqLevel] == false);
+	if(request[reqLevel-1][direction ? 0 : 1] == false && destination[reqLevel-1] == false);
 		nonPossibleStop++;
 }
 
 void chooseDestination(int level){
+	callElevator(level);
 	addStop(level);
+	destination[level-1] = true;
+}
+
+void requestUp(int level){
+	callElevator(level);
+	addStop(level, UP);
+	request[level-1][0] = true;
+}
+
+void requestDown(int level){
+	callElevator(level);
+	addStop(level, DOWN);
+	request[level-1][1] = true;
 }
 
 void optoSensorInterrupt(){
 	if(distanceCount == 5){
+		distanceCount = 0;
 		currentLevel = addCurrentLevel();
-		if(destination[currentLevel] == true || request[currentLevel][currentDirection] == true){
-			request[currentLevel][currentDirection] = false;
-			destination[currentLevel] = false;
+		if(destination[currentLevel-1] == true || request[currentLevel-1][currentDirection] == true){
+			request[currentLevel-1][currentDirection] = false;
+			destination[currentLevel-1] = false;
 			if(--possibleStop == 0){
 				isElevatorOn = false;
 				PWM1.pulsewidth_us(0);
@@ -103,34 +120,47 @@ void optoSensorInterrupt(){
 			}
 		}
 		currentDirection = checkLimitDirection();
-		distanceCount = 0;
 	}
 	distanceCount++;
 }
 
 void buttonUp1Interrupt(){
-	callElevator(1);
-	addStop(1, UP);
-	request[0][0] = true;
+	requestUp(1);
 }
 
 void buttonUp2Interrupt(){
-	callElevator(2);
-	addStop(2, UP);
-	request[2][0] = true;
+	requestUp(2);
 }
 
 void buttonDown2Interrupt(){
-	callElevator(2);
-	addStop(2, DOWN);
-	request[2][1] = true;
+	requestDown(2);
 }
 
 void buttonDown3Interrupt(){
-	callElevator(3);
-	addStop(3, DOWN);
-	request[3][1] = true;
+	requestDown(3);
 }
+
+void keyPadCol1Interrupt(){
+	if(!keyPadInterruptState)
+		chooseDestination(1);
+	keyPadInterruptState = false;
+}
+
+void keyPadCol2Interrupt(){
+	if(!keyPadInterruptState){
+		chooseDestination(2);
+		keyPadInterruptState = true;
+	}
+}
+
+void keyPadCol3Interrupt(){
+	if(!keyPadInterruptState){
+		chooseDestination(3);
+		keyPadInterruptState = true;
+	}
+}
+
+
 
 int main() {
 	PWM1.period(0.010);
@@ -139,6 +169,10 @@ int main() {
 	buttonUp2.fall(&buttonUp2Interrupt);
 	buttonDown2.fall(&buttonDown2Interrupt);
 	buttonDown3.fall(&buttonDown3Interrupt);
+	keyPadCol1.fall(&keyPadCol1Interrupt);
+	keyPadCol2.fall(&keyPadCol2Interrupt);
+	keyPadCol3.fall(&keyPadCol3Interrupt);
+	keyPadRow1 = 1;
 	while(1){
 
 	}
